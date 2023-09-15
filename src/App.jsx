c851f97f8d2eab1c5078396d31481013612a7fec
Lucas Zemlin <lzemlin@LABA.PSWAS.LOCAL>
2023-09-15 15:21:02 -0700

    template for learing useContext

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+// STATE thats just for app.jsx
+
+  return (
+    <div>
+      <h1>My Context App</h1>
+      <Home />
+    </div>
+  )
+}
+
+export default App
